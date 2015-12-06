2c7401236e982ad43ed70c20c719a606c4589429
drut11 <darrylrutledge@gmail.com>
2015-12-05 23:34:43 -0500

    I pushed the shit out of this

---
+++ app/src/main/java/com/example/ddw/pmd/DBAdapter.java
@@ -0,0 +1,246 @@
+package com.example.ddw.pmd;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Darryl on 12/5/2015.
+ */
+public class DBAdapter {
+
+    static final String TAG = "DBAdapter";
+    static final String DATABASE_NAME = "PMDdb";
+    static final int DATABASE_VERSION = 1;
+    final Context context;
+
+    DatabaseHelper DBHelper;
+    SQLiteDatabase db;
+    String currentTable = "";
+
+    public DBAdapter(Context ctx)
+    {
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper
+    {
+        DatabaseHelper(Context context)
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db)
+        {
+            try {
+                db.execSQL(DBContract.CREATE_DB);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS contacts");
+            onCreate(db);
+        }
+    }
+
+    //---opens the database---
+    public DBAdapter open() throws SQLException
+    {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    //---closes the database---
+    public void close() { DBHelper.close(); }
+
+    //---retrieves all the users---
+    public Cursor getAllUsers()
+    {
+        return db.query(DBContract.UserInfo.USER_TABLE, new String[] {
+                DBContract.UserInfo.USER_ID,
+                DBContract.UserInfo.USER_USERNAME,
+                DBContract.UserInfo.USER_PASSWORD,
+                DBContract.UserInfo.USER_FIRSTNAME,
+                DBContract.UserInfo.USER_LASTNAME,
+                DBContract.UserInfo.USER_USERTYPE,
+                DBContract.UserInfo.USER_EMAIL,
+                DBContract.UserInfo.USER_MEALPLAN,
+                DBContract.UserInfo.USER_WORKOUTPLAN},                       
+                null, null, null, null, null);
+    }
+
+    //---retrieves a particular user---
+    public Cursor getUser(long id) throws SQLException {
+        Cursor mCursor =
+                db.query(true, DBContract.UserInfo.USER_TABLE, new String[] {
+                        DBContract.UserInfo.USER_ID,
+                        DBContract.UserInfo.USER_USERNAME,
+                        DBContract.UserInfo.USER_PASSWORD,
+                        DBContract.UserInfo.USER_FIRSTNAME,
+                        DBContract.UserInfo.USER_LASTNAME,
+                        DBContract.UserInfo.USER_USERTYPE,
+                        DBContract.UserInfo.USER_EMAIL,
+                        DBContract.UserInfo.USER_MEALPLAN,
+                        DBContract.UserInfo.USER_WORKOUTPLAN},
+                        DBContract.UserInfo.USER_ID + "=" + id, 
+                        null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+    
+    //---insert a user into the database---
+    public long addUser(String username, String password, String firstname, String lastname, String usertype, String email, int mealplan, int workoutplan) {
+        currentTable = DBContract.UserInfo.USER_TABLE;
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(DBContract.UserInfo.USER_USERNAME, username);
+        initialValues.put(DBContract.UserInfo.USER_PASSWORD, password);
+        initialValues.put(DBContract.UserInfo.USER_FIRSTNAME, firstname);
+        initialValues.put(DBContract.UserInfo.USER_LASTNAME, lastname);
+        initialValues.put(DBContract.UserInfo.USER_USERTYPE, usertype);
+        initialValues.put(DBContract.UserInfo.USER_EMAIL, email);
+        initialValues.put(DBContract.UserInfo.USER_MEALPLAN, mealplan);
+        initialValues.put(DBContract.UserInfo.USER_WORKOUTPLAN, workoutplan);
+        return db.insert(currentTable, null, initialValues);
+    }
+
+    //---update user in database---
+    public boolean updateUser(long id, String username, String password, String firstname, String lastname, String usertype, String email, int mealplan, int workoutplan) {
+        currentTable = DBContract.UserInfo.USER_TABLE;
+        ContentValues values = new ContentValues();
+        values.put(DBContract.UserInfo.USER_USERNAME, username);
+        values.put(DBContract.UserInfo.USER_PASSWORD, password);
+        values.put(DBContract.UserInfo.USER_FIRSTNAME, firstname);
+        values.put(DBContract.UserInfo.USER_LASTNAME, lastname);
+        values.put(DBContract.UserInfo.USER_USERTYPE, usertype);
+        values.put(DBContract.UserInfo.USER_EMAIL, email);
+        values.put(DBContract.UserInfo.USER_MEALPLAN, mealplan);
+        values.put(DBContract.UserInfo.USER_WORKOUTPLAN, workoutplan);
+        return db.update(currentTable, values, DBContract.UserInfo.USER_ID + " = " + id, null) > 0;
+    }
+
+    //---deletes a particular user---
+    public boolean deleteUser(long id)
+    {
+        return db.delete(DBContract.UserInfo.USER_TABLE, DBContract.UserInfo.USER_ID + "=" + id, null) > 0;
+    }
+
+    //---retrieves all the users---
+    public Cursor getAllMealplans() {
+        return db.query(DBContract.MealPlanInfo.MEALPLAN_TABLE, new String[] {
+                        DBContract.MealPlanInfo.MEALPLAN_ID,
+                        DBContract.MealPlanInfo.MEALPLAN_PLANNAME,
+                        DBContract.MealPlanInfo.MEALPLAN_DESCRIPTION,
+                        DBContract.MealPlanInfo.MEALPLAN_DETAILS},
+                null, null, null, null, null);
+    }
+
+    //---retrieves a particular meal plan---
+    public Cursor getMealplan(long id) throws SQLException {
+        Cursor mCursor =
+                db.query(true, DBContract.MealPlanInfo.MEALPLAN_TABLE, new String[] {
+                                DBContract.MealPlanInfo.MEALPLAN_ID,
+                                DBContract.MealPlanInfo.MEALPLAN_PLANNAME,
+                                DBContract.MealPlanInfo.MEALPLAN_DESCRIPTION,
+                                DBContract.MealPlanInfo.MEALPLAN_DETAILS},
+                        DBContract.MealPlanInfo.MEALPLAN_ID + "=" + id,
+                        null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+    
+    
+    //---insert a meal plan into the database---
+    public long addMealplan(String name, String desc, String detail) {
+        currentTable = DBContract.MealPlanInfo.MEALPLAN_TABLE;
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(DBContract.MealPlanInfo.MEALPLAN_PLANNAME, name);
+        initialValues.put(DBContract.MealPlanInfo.MEALPLAN_DESCRIPTION, desc);
+        initialValues.put(DBContract.MealPlanInfo.MEALPLAN_DETAILS, detail);
+        return db.insert(currentTable, null, initialValues);
+    }
+
+    //---update meal plan in database---
+    public boolean updateMealplan (long id, String name, String desc, String detail) {
+        currentTable = DBContract.MealPlanInfo.MEALPLAN_TABLE;
+        ContentValues values = new ContentValues();
+        values.put(DBContract.MealPlanInfo.MEALPLAN_PLANNAME, name);
+        values.put(DBContract.MealPlanInfo.MEALPLAN_DESCRIPTION, desc);
+        values.put(DBContract.MealPlanInfo.MEALPLAN_DETAILS, detail);
+        return db.update(currentTable, values, DBContract.MealPlanInfo.MEALPLAN_ID + " = " + id, null) > 0;
+    }
+
+    //---deletes a particular meal plan---
+    public boolean deleteMealplan(long id)
+    {
+        return db.delete(DBContract.MealPlanInfo.MEALPLAN_TABLE, DBContract.MealPlanInfo.MEALPLAN_ID + "=" + id, null) > 0;
+    }
+
+    //---retrieves all the workout plans---
+    public Cursor getAllWorkoutplans() {
+        return db.query(DBContract.WorkoutPlanInfo.WORKOUTPLAN_TABLE, new String[] {
+                        DBContract.WorkoutPlanInfo.WORKOUTPLAN_ID,
+                        DBContract.WorkoutPlanInfo.WORKOUTPLAN_PLANNAME,
+                        DBContract.WorkoutPlanInfo.WORKOUTPLAN_DESCRIPTION,
+                        DBContract.WorkoutPlanInfo.WORKOUTPLAN_DETAILS},
+                null, null, null, null, null);
+    }
+
+    //---retrieves a particular workout plan---
+    public Cursor getWorkoutplan(long id) throws SQLException {
+        Cursor mCursor =
+                db.query(true, DBContract.WorkoutPlanInfo.WORKOUTPLAN_TABLE, new String[] {
+                                DBContract.WorkoutPlanInfo.WORKOUTPLAN_ID,
+                                DBContract.WorkoutPlanInfo.WORKOUTPLAN_PLANNAME,
+                                DBContract.WorkoutPlanInfo.WORKOUTPLAN_DESCRIPTION,
+                                DBContract.WorkoutPlanInfo.WORKOUTPLAN_DETAILS},
+                        DBContract.WorkoutPlanInfo.WORKOUTPLAN_ID + "=" + id,
+                        null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+
+    //---insert a workout plan into the database---
+    public long addWorkoutplan(String name, String desc, String detail) {
+        currentTable = DBContract.WorkoutPlanInfo.WORKOUTPLAN_TABLE;
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(DBContract.WorkoutPlanInfo.WORKOUTPLAN_PLANNAME, name);
+        initialValues.put(DBContract.WorkoutPlanInfo.WORKOUTPLAN_DESCRIPTION, desc);
+        initialValues.put(DBContract.WorkoutPlanInfo.WORKOUTPLAN_DETAILS, detail);
+        return db.insert(currentTable, null, initialValues);
+    }
+
+    //---update workout plan in database ---
+    public boolean updateWorkoutplan(long id, String name, String desc, String detail) {
+        currentTable = DBContract.WorkoutPlanInfo.WORKOUTPLAN_TABLE;
+        ContentValues values = new ContentValues();
+        values.put(DBContract.WorkoutPlanInfo.WORKOUTPLAN_PLANNAME, name);
+        values.put(DBContract.WorkoutPlanInfo.WORKOUTPLAN_DESCRIPTION, desc);
+        values.put(DBContract.WorkoutPlanInfo.WORKOUTPLAN_DETAILS, detail);
+        return db.update(currentTable, values, DBContract.WorkoutPlanInfo.WORKOUTPLAN_ID + " = " + id, null) > 0;
+    }
+
+    //---deletes a particular workout plan---
+    public boolean deleteWorkoutplan(long id)
+    {
+        return db.delete(DBContract.WorkoutPlanInfo.WORKOUTPLAN_TABLE, DBContract.WorkoutPlanInfo.WORKOUTPLAN_ID + "=" + id, null) > 0;
+    }
+}
---
+++ app/src/main/java/com/example/ddw/pmd/DBContract.java
@@ -0,0 +1,67 @@
+package com.example.ddw.pmd;
+
+/**
+ * Created by Darryl on 12/5/2015.
+ */
+public class DBContract {
+
+    public static final String CREATE_DB =
+            UserInfo.CREATETABLE_USERS + MealPlanInfo.CREATETABLE_MEALPLANS + WorkoutPlanInfo.CREATETABLE_WORKOUTPLANS;
+
+
+    public static abstract class UserInfo {
+        public static final String USER_TABLE = "users";
+        public static final String USER_ID = "userid";
+        public static final String USER_USERNAME = "username";
+        public static final String USER_PASSWORD = "password";
+        public static final String USER_FIRSTNAME = "firstname";
+        public static final String USER_LASTNAME = "lastname";
+        public static final String USER_USERTYPE = "usertype";
+        public static final String USER_EMAIL = "email";
+        public static final String USER_MEALPLAN = "mealplan";
+        public static final String USER_WORKOUTPLAN = "workoutplan";
+
+        public static final String CREATETABLE_USERS =
+                "create table " + USER_TABLE + " (" +
+                        USER_ID + " integer primary key autoincrement, " +
+                        USER_USERNAME + " text not null, " +
+                        USER_PASSWORD + " text not null, " +
+                        USER_FIRSTNAME + " text not null, " +
+                        USER_LASTNAME + " text not null, " +
+                        USER_USERTYPE + " integer not null, " +
+                        USER_EMAIL + " text not null, " +
+                        USER_MEALPLAN + " integer, " +
+                        USER_WORKOUTPLAN + " integer);";
+    }
+
+    public static abstract class MealPlanInfo {
+        public static final String MEALPLAN_TABLE = "mealplans";
+        public static final String MEALPLAN_ID = "mealplanid";
+        public static final String MEALPLAN_PLANNAME = "planname";
+        public static final String MEALPLAN_DESCRIPTION = "description";
+        public static final String MEALPLAN_DETAILS = "details";
+
+        public static final String CREATETABLE_MEALPLANS =
+                "create table " + MEALPLAN_TABLE + " (" +
+                        MEALPLAN_ID + " integer primary key autoincrement, " +
+                        MEALPLAN_PLANNAME + " text not null, " +
+                        MEALPLAN_DESCRIPTION + " text not null, " +
+                        MEALPLAN_DETAILS + " text not nul);";
+    }
+
+    public static abstract class WorkoutPlanInfo {
+        public static final String WORKOUTPLAN_TABLE = "workoutplans";
+        public static final String WORKOUTPLAN_ID = "workoutpplanid";
+        public static final String WORKOUTPLAN_PLANNAME = "planname";
+        public static final String WORKOUTPLAN_DESCRIPTION = "description";
+        public static final String WORKOUTPLAN_DETAILS = "details";
+
+        public static final String CREATETABLE_WORKOUTPLANS =
+                "create table " + WORKOUTPLAN_TABLE + " (" +
+                        WORKOUTPLAN_ID + " integer primary key autoincrement, " +
+                        WORKOUTPLAN_PLANNAME + " text not null, " +
+                        WORKOUTPLAN_DESCRIPTION + " text not null, " +
+                        WORKOUTPLAN_DETAILS + " text not null);";
+    }
+
+}
